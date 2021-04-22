6c11ebc46e620f50a1b60a0e7fdaa54b21ef5f8a
Alswl_ya <71184811+RapunzeI@users.noreply.github.com>
2021-04-22 22:50:59 +0900

    Delete 36.py

--- 36.py
+++
@@ -1,9 +0,0 @@
-def reverse_int(n):
-    first = n//100
-    second = (n-first*100)//10
-    third = n % 10
-    return third*100+second*10+first
-
-
-print(reverse_int(123))
-print(reverse_int(908))
